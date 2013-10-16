affa331b60a61c7a9480f2e81e36373a7e11d78e
seiji-k <zeichk.alt@gmail.com>
2013-10-16 16:16:52 +0900

    RESTでPostegresからデータを検索して、持ってくるところまで実装できました。

---
+++ src/main/java/com/kddi/tsugai/webapi/dao/JdbcUserDao.java
@@ -0,0 +1,51 @@
+package com.kddi.tsugai.webapi.dao;
+
+import com.kddi.tsugai.webapi.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.postgresql.Driver;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: seiji-k
+ * Date: 2013/10/16
+ * Time: 13:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class JdbcUserDao {
+    private DriverManagerDataSource dataSource=new DriverManagerDataSource();
+    private User user = new User();
+
+    public void setDataSource() {
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://ec2-54-238-140-97.ap-northeast-1.compute.amazonaws.com:5432/tsugai_proto");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+    }
+
+    public User get(String corpId,String userId) throws SQLException {
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        return jdbcTemplate.queryForObject("select * from t_user where corp_id =? and user_id = ?",new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                User u = new User();
+                u.setCorpId(resultSet.getString("corp_id"));
+                u.setId(resultSet.getString("user_id"));
+                u.setProperties(resultSet.getString("properties"));
+                return u;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        },corpId,userId);
+    }
+
+
+
+}
--- src/main/java/com/kddi/tsugai/webapi/service/UserServiceImpl.java
+++ src/main/java/com/kddi/tsugai/webapi/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kddi.tsugai.webapi.service;
 
+import com.kddi.tsugai.webapi.dao.JdbcUserDao;
 import com.kddi.tsugai.webapi.domain.User;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -7,6 +8,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,16 +19,25 @@ import java.io.IOException;
  */
 @Service
 public class UserServiceImpl implements UserService {
+    JdbcUserDao jdbcUserDao = new JdbcUserDao();
 
     @Override
     public User get(String corpId,String userId){
         User user = new User();
-        user.setId(userId);
+/*      user.setId(userId);
         user.setCorpId(corpId);
         user.setPropertyMap("phone", "080-5066-9380");
         user.setPropertyMap("mailAddress", "seiji-k@kddi.com");
         user.setPropertyMap("password","password");
         user.setProperties();
+        return user;*/
+        try {
+            jdbcUserDao.setDataSource();
+            return jdbcUserDao.get(corpId,userId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("error jdbcUserDao.get");//To change body of catch statement use File | Settings | File Templates.
+        }
         return user;
     }
 
